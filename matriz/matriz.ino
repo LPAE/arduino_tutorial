e77c7bd4aa1e8702ce2932c8dd9be392c054f001
ingridJulia <89813681+ingridJulia@users.noreply.github.com>
2021-12-10 01:10:34 -0300

    Add files via upload

---
+++ matriz/matriz.ino
@@ -0,0 +1,20 @@
+// Inclui a biblioteca
+#include <MatrizLed.h>
+
+// Nomeando a matriz de led
+MatrizLed matriz;
+
+// Função setup é executada apenas uma vez
+void setup() {
+  // dataPin, clkPin, csPin, numero de matrizes de 8x8
+  matriz.begin(10, 8, 9, 2); 
+  // Caso ocorra falha
+  matriz.rotar(false); 
+}
+
+// Função loop é para executar repetidamente o código
+void loop() {
+  matriz.borrar();
+  // Exibe o texto na matriz de led
+  matriz.escribirFraseScroll("Testando", 200); 
+}
